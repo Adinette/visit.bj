56dafa794f4c9ff2a9f7bc43a574cc993e7f8b5d
Adinette Dougloui <adinette.dougloui@intside.com>
2023-06-12 11:32:11 +0100

    first commit

--- visit.bj/src/router/index.js
+++ visit.bj/src/router/index.js
@@ -1,23 +1,40 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
+import LoginView from '../views/LoginView.vue'
+import SignupView from '../views/SignupView.vue'
+import Forgot_passwordView from '../views/Forgot_passwordView.vue'
+import NewpassswordView from '../views/NewpassswordView.vue'
+import VerificationView from '../views/VerificationView.vue'
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: '/signup',
+      name: 'signup',
+      component: SignupView
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+      path: '/login',
+      name: 'login',
+      component: LoginView
+    },
+    {
+      path: '/forgot_password',
+      name: 'forgot_password',
+      component: Forgot_passwordView
+    },
+    {
+      path: '/newpasssword',
+      name: 'newpasssword',
+      component: NewpassswordView
+    },
+    {
+      path: '/verification',
+      name: 'verification',
+      component: VerificationView
+    },
   ]
+  
 })
 
 export default router

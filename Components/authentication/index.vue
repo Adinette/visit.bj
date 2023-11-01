f2e3361c770ad7292fc7d89167801f6102bfe1b3
Lucas Tamomo <tamomo.lucas@gmail.com>
2023-11-01 18:00:43 +0100

    Add index login

---
+++ Components/authentication/index.vue
@@ -0,0 +1,18 @@
+<script setup></script>
+
+<template>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 mx-auto">
+          <div class="blogAuthentication p-3 p-md-5">
+            <div class="text-center mb-4"><img src="~/assets/images/logo.png" alt="logo"></div>
+            <div>
+              <h2>Se connecter</h2>
+              <p>Découvrez le Bénin à travers nos offres de voyage.</p>
+              <slot></slot> 
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+</template>

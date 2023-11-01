d336b6bd3678309feb9fe9649a443b6c36321e86
Lucas Tamomo <tamomo.lucas@gmail.com>
2023-11-01 16:48:25 +0100

    Update style

--- Components/authentication/ LoginForm.vue
+++ Components/authentication/ LoginForm.vue
@@ -1,42 +1,18 @@
 <template>
-  <form @submit.prevent="handleSubmit">
-    <InputField type="email" label="Email" id="email" :validator="emailValidator" v-model="email" />
-    <InputField type="password" label="Password" id="password" :validator="passwordValidator" v-model="password" />
+  <form>
+    <div class="mb-3"><InputField type="email" label="Email" v-model="email" /></div>
+    <div class="mb-3"><InputField type="password" label="Password"  v-model="password" /></div>
+    <div class="text-end mb-3"><NuxtLink to="#">Mot de pase oublié?</NuxtLink></div>
     <button type="submit">Login</button>
     <p v-if="feedback">{{ feedback }}</p>
   </form>
 </template>
 
-<script>
+<script setup>
 import InputField from './InputField.vue';
 
-export default {
-  components: {
-    InputField
-  },
-  data() {
-    return {
-      email: '',
-      password: '',
-      feedback: ''
-    };
-  },
-  methods: {
-    emailValidator(email) {
-      if (!email) return 'Email is required.';
-      const re = /\S+@\S+\.\S+/;
-      if (!re.test(email)) return 'Email is invalid.';
-      return null;
-    },
-    passwordValidator(password) {
-      if (!password) return 'Password is required.';
-      if (password.length < 6) return 'Password is too short.';
-      return null;
-    },
-    handleSubmit() {
-      this.feedback = 'Login successful'; // you can update this based on your backend response
-    }
-  }
-};
+ 
 </script>
+
+
 
--- Components/authentication/InputField.vue
+++ Components/authentication/InputField.vue
@@ -1,39 +1,29 @@
+<script setup>
+import { ref, defineProps } from 'vue';
+
+const props = defineProps({
+  label: String,
+  type: String,
+  value: String,
+  error: String
+});
+
+const valueRef = ref(props.value);
+
+watch(() => {
+  valueRef.value = props.value;
+});
+
+</script>
+
 <template>
-  <div>
-    <label :for="id">{{ label }}</label>
-    <input :type="type" :id="id" v-model="value" @input="onInput" @blur="validate" />
-    <p v-if="error" class="error-text">{{ error }}</p>
+  <div class="position-relative">
+    <input class="form-control floatingInput" :type="props.type" :id="props.id" v-model="valueRef" />
+    <label class="floatingLabel">{{ props.label }}</label>
+    <p v-if="props.error" class="error-text">{{ props.error }}</p>
   </div>
 </template>
 
-<script>
-export default {
-  props: ['type', 'label', 'id', 'validator'],
-  emits: ['update:modelValue'],
-  data() {
-    return {
-      value: '',
-      error: null
-    };
-  },
-  methods: {
-    onInput() {
-      this.error = null;
-      this.$emit('update:modelValue', this.value);
-    },
-    validate() {
-      if (this.validator) {
-        const error = this.validator(this.value);
-        this.error = error;
-      }
-    }
-  }
-};
-</script>
+<style scoped> </style>
 
-<style scoped>
-.error-text {
-  color: red;
-}
-</style>
 

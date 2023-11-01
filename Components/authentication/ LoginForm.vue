d5dd3fb8a637f580dc005e591a7a953b73e026b5
Lucas Tamomo <tamomo.lucas@gmail.com>
2023-11-01 18:00:13 +0100

    update login

--- Components/authentication/ LoginForm.vue
+++ Components/authentication/ LoginForm.vue
@@ -1,18 +1,14 @@
+<script setup>
+import InputField from '../widgets/input/InputField.vue';
+ 
+</script>
+
 <template>
   <form>
-    <div class="mb-3"><InputField type="email" label="Email" v-model="email" /></div>
-    <div class="mb-3"><InputField type="password" label="Password"  v-model="password" /></div>
+    <div class="mb-3"><InputField type="email" label="E-mail" v-model="email" placeholder="E-mail" /></div>
+    <div class="mb-3"><InputField type="password" label="Mot de passe"  v-model="password"  placeholder="Mot de passe" /></div>
     <div class="text-end mb-3"><NuxtLink to="#">Mot de pase oublié?</NuxtLink></div>
     <button type="submit">Login</button>
     <p v-if="feedback">{{ feedback }}</p>
   </form>
 </template>
-
-<script setup>
-import InputField from './InputField.vue';
-
- 
-</script>
-
-
-

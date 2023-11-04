62d549103e8c278b7e50b1616d42cc6c1aaae6cc
Aristote O <oaristote@gmail.com>
2023-11-04 18:04:21 +0100

    feat: remove and add assets css image

---
+++ assets/images/index.ts
@@ -0,0 +1,29 @@
+import articlehistory from "./articlehistory.png";
+import articlehistory2 from "./articlehistory2.png";
+import articlehistory3 from "./articlehistory3.png";
+import discorver_1 from "./discorver_1.png";
+import discorver_2 from "./discorver_2.png";
+import discorver_3 from "./discorver_3.png";
+import imagebg from "./imagebackground.png";
+import logo from "./logo.png";
+import facebook from "./Logofacebook.png";
+import google from "./Logogoogle.png";
+import newsletter from "./newsletter.png";
+import pack_1 from "./pack_1.png";
+import pack_2 from "./pack_2.png";
+
+export {
+  articlehistory,
+  articlehistory2,
+  articlehistory3,
+  discorver_1,
+  discorver_2,
+  discorver_3,
+  imagebg,
+  logo,
+  facebook,
+  google,
+  newsletter,
+  pack_1,
+  pack_2,
+};
